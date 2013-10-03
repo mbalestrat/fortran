1bade2799ab0d14ba1e4906b7f76a261f969a614
Bartosz Szczesny <bszcz@bszcz.eu>
2013-10-03 19:42:29 +0100

    updated task_3.f95 to work on new sound.txt file (also added)

--- intro_course/task_3.f95
+++ intro_course/task_3.f95
@@ -2,56 +2,53 @@
 ! LICENSE: The MIT License (MIT)
 
 program task3
-    !logical :: seat_v( 300 )
-    real :: sound( 300, 7200 )
+    logical, parameter :: check_laudness = .false.
+    integer, parameter :: nseats = 30, nsecs = 720
     integer :: seat, seat_l, seat_q, time, sec
-
+    real :: sound( nseats, nsecs )
 
     ! read data
     open( unit = 22, file = "sound.txt" )
-    do seat = 1, 300
-        do sec = 1, 7200
-            read( 22, * ) sound( seat, sec )
-        enddo
+    do seat = 1, nseats
+            read( 22, * ) ( sound( seat, sec ), sec = 1, nsecs )
     enddo
     close( 22 )
 
-    goto 13 ! skip laudness check as it prints lots of lines
-
     ! check laudness
-    do seat = 1, 300
-        do sec = 1, 7200
-            if ( sound( seat, sec ) > 90.0 ) then
-                print *, 'seat=', seat, ' sec=', sec, ' sound=', sound( seat, sec ), ' > 90 !'
-            endif
-            if ( sound( seat, sec ) < 10.0 ) then
-                print *, 'seat=', seat, ' sec=', sec, ' sound=', sound( seat, sec ), ' < 10 !'
-            endif
+    if ( check_laudness ) then
+        do seat = 1, nseats
+            do sec = 1, nsecs
+                if ( sound( seat, sec ) > 90.0 ) then
+                    print *, 'seat =', seat, ' sec =', sec, ' sound =', sound( seat, sec ), ' > 90 !'
+                endif
+                if ( sound( seat, sec ) < 10.0 ) then
+                    print *, 'seat =', seat, ' sec =', sec, ' sound =', sound( seat, sec ), ' < 10 !'
+                endif
+            enddo
         enddo
-    enddo
+    endif
 
-13  continue ! laudness check skipped
-    ! find laudest and quitest at 50min = 3000sec
+    ! find laudest and quitest at 5min = 300sec
     seat_l = 1
     seat_q = 1
-    time = 50 * 60
-    do seat = 2, 300
+    time = 5 * 60
+    do seat = 1, nseats
         if ( sound( seat, time ) > sound( seat_l, time ) ) then
             seat_l = seat
         endif
-        if ( sound( seat, time ) < sound( seat_l, time ) ) then
+        if ( sound( seat, time ) < sound( seat_q, time ) ) then
             seat_q = seat
         endif
     enddo
-    print *, '@ time=50min'
-    print *, ' laudest_seat=', seat_l, ' sound=', sound( seat_l, time )
-    print *, 'quietest_seat=', seat_q, ' sound=', sound( seat_q, time )
+    print *, '@ time = 5min'
+    print *, ' laudest_seat =', seat_l, ' sound =', sound( seat_l, time )
+    print *, 'quietest_seat =', seat_q, ' sound =', sound( seat_q, time )
 
     ! check laudness but now only print seat number once
-    do seat = 1, 300
-        do sec = 1, 7200
+    do seat = 1, nseats
+        do sec = 1, nsecs
             if ( sound( seat, sec ) > 90.0 .or. sound( seat, sec ) < 10.0 ) then
-                print *, 'violation at seat=', seat
+                print "( a, i2 )", 'violation at seat = ', seat
                 exit
             endif
         enddo
