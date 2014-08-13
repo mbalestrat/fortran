4cd19247c2f35237b74ed4c61b7a9c9d4d1fc012
Bartosz Szczesny <bszcz@bszcz.org>
2014-08-13 20:55:26 +0100

    correct old comment

--- extra_bits/common_blocks.f95
+++ extra_bits/common_blocks.f95
@@ -1,5 +1,5 @@
-! Copyright (c) 2012-2013 Bartosz Szczesny
-! LICENSE: The MIT License (MIT)
+! Copyright (c) 2012-2013 Bartosz Szczesny <bszcz@bszcz.org>
+! This program is free software under the MIT license.
 
 block data bd
     real :: x = 0.123
@@ -8,21 +8,20 @@ end block data
 
 program common_blocks
     interface
-        function x_plus_y( y )
+        function x_plus_y(y)
             real :: x_plus_y, y
         end function
     end interface
 
     real :: y = 0.654
-    print "( a, f5.3 )", "x_plus_y = ", x_plus_y( y )
+    print "(a, f5.3)", "x_plus_y = ", x_plus_y(y)
 
     stop
 end
 
-real function x_plus_y( y )
-    real :: x, y
-    common / c / x
-    ! no need to pass 'j' and 'y' as arguments
+real function x_plus_y(y)
+    real :: x, y   ! don't need to pass 'x' as argument
+    common / c / x ! 'x' is shared via common block 'c'
     x_plus_y = x + y
     return
 end
